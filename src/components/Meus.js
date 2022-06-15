e2355e25905528f8c7bcbff50dd48240b9b47ee5
SueYeong <dudtb205@gmail.com>
2022-06-15 13:40:51 +0900

    ex_1

--- src/components/Meus.js
+++ src/components/Meus.js
@@ -1,8 +1,9 @@
 export const Menus = ({ menu }) => {
   return (
     <>
-      <h1>오늘의 메뉴</h1>
       <ul>
+        <h1>오늘의 메뉴</h1>
+        <h3>선택사항</h3>
         <li>
           오늘의 메뉴는 {menu[0].menu} 사이드는 {menu[0].side}
         </li>
@@ -18,6 +19,7 @@ export const Menus = ({ menu }) => {
         <li>
           오늘의 메뉴는 {menu[4].menu} 사이드는 {menu[4].side}
         </li>
+        <h3>고르든지 말들지</h3>
       </ul>
     </>
   );
