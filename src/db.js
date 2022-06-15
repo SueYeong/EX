55d8310e3e56a7cc7f073dc63f738a91ba7f7756
SueYeong <dudtb205@gmail.com>
2022-06-15 13:22:15 +0900

    EX

---
+++ src/db.js
@@ -0,0 +1,27 @@
+export const TodayMenu = [
+  {
+    id: 0,
+    menu: "텐동",
+    side: "에이드",
+  },
+  {
+    id: 1,
+    menu: "족발",
+    side: "콜라",
+  },
+  {
+    id: 2,
+    menu: "갈비찜",
+    side: "쿨피스",
+  },
+  {
+    id: 3,
+    menu: "모듬전",
+    side: "막걸리",
+  },
+  {
+    id: 4,
+    menu: "밀면",
+    side: "만두",
+  },
+];
